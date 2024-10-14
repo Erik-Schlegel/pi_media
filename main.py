2642bbef701af37dc153c4ad7336140a3fe96305
Erik Schlegel <epschlegel@gmail.com>
2024-10-14 22:11:18 +0000

    Switch to chromium-browser for framerate.

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
-import webview
 import os
+import subprocess
 
-class Api:
-    def say_hello(self):
-        return "Hello from Python!"
-
-api = Api()
-print("Starting PyWebview...")
-
-# Use absolute path for the HTML file
 html_file = os.path.abspath('index.html')
+# Launch Chromium in kiosk mode to display the HTML
+# subprocess.run(['chromium-browser', '--kiosk', '--autoplay-policy=no-user-gesture-required', html_file])
+# chromium-browser --disable-web-security --user-data-dir="/tmp/chrome_dev" /home/media/eschware/pi_media/index.html
+# subprocess.run(['chromium-browser', '--disable-web-security', html_file])
 
-# Create the window without trying to do anything before it exists
-window = webview.create_window('My App', html_file, js_api=api, fullscreen=True, frameless=True)
-
-# Define a function to run once the window has finished loading
-def on_loaded():
-    try:
-        print("Content loaded, attempting to refresh window...")
-        # Force a refresh by resizing or simulating a resize event
-        window.evaluate_js("window.dispatchEvent(new Event('resize'));")
-    except webview.WebViewException as e:
-        print(f"WebViewException occurred: {e}")
-
-print("Window created, starting GUI...")
-# Start the webview GUI, specifying the on_loaded callback
-webview.start(on_loaded)
+subprocess.run([
+    'chromium-browser',
+    '--disable-web-security',
+    '--kiosk',
+    '--allow-file-access-from-files',
+    '--user-data-dir="/tmp/chrome_dev"',
+    'file:///home/media/eschware/pi_media/index.html'
+])
