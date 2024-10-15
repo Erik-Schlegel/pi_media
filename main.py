b82ddfad57595cd5fc362fa1afd8c48875dc2c65
Erik Schlegel <epschlegel@gmail.com>
2024-10-15 01:20:16 +0000

    Open chromium-browser as maximized kiosk.

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import os
 import subprocess
 
-html_file = os.path.abspath('index.html')
-# Launch Chromium in kiosk mode to display the HTML
-# subprocess.run(['chromium-browser', '--kiosk', '--autoplay-policy=no-user-gesture-required', html_file])
-# chromium-browser --disable-web-security --user-data-dir="/tmp/chrome_dev" /home/media/eschware/pi_media/index.html
-# subprocess.run(['chromium-browser', '--disable-web-security', html_file])
-
 subprocess.run([
     'chromium-browser',
-    '--disable-web-security',
+    '--start-maximized',
+    '--start-fullscreen',
     '--kiosk',
     '--allow-file-access-from-files',
-    '--user-data-dir="/tmp/chrome_dev"',
+    '--user-data-dir=/tmp/another_chrome_dev',
     'file:///home/media/eschware/pi_media/index.html'
 ])
